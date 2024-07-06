5286bebe9b8771b2f7824967035ad8d0d572671d
qzhhhi <zihanqin2048@gmail.com>
2024-07-06 22:22:52 +0000

    Add watchdog for key referee system messages Adapt to new independent rmcs_msgs

--- src/command/interaction/header.hpp
+++ src/command/interaction/header.hpp
@@ -2,14 +2,14 @@
 
 #include <cstdint>
 
-#include "rmcs_referee/msg.hpp"
+#include <rmcs_msgs/full_robot_id.hpp>
 
 namespace rmcs_referee::command::interaction {
 
 struct __attribute__((packed)) Header {
     uint16_t command_id;
-    rmcs_referee::msg::Id sender_id;
-    rmcs_referee::msg::Id receiver_id;
+    uint16_t sender_id;
+    uint16_t receiver_id;
 };
 
 } // namespace rmcs_referee::command::interaction
--- src/status.cpp
+++ src/status.cpp
@@ -1,8 +1,11 @@
 #include <rclcpp/node.hpp>
 
 #include <rmcs_executor/component.hpp>
+#include <rmcs_msgs/game_stage.hpp>
+#include <rmcs_msgs/robot_id.hpp>
 #include <serial_util/crc/dji_crc.hpp>
 #include <serial_util/package_receive.hpp>
+#include <serial_util/tick_timer.hpp>
 
 #include "frame.hpp"
 #include "status/field.hpp"
@@ -26,11 +29,17 @@ public:
             RCLCPP_ERROR(logger_, "Unable to open serial port: %s", ex.what());
         }
 
-        register_output("/referee/robot/shooter/cooling", robot_shooter_cooling_, 0);
-        register_output("/referee/robot/shooter/heat_limit", robot_shooter_heat_limit_, 0);
-        register_output("/referee/robot/chassis_power", robot_chassis_power_, 0.0);
-        register_output("/referee/robot/chassis_power_limit", robot_chassis_power_limit_, 45.0);
-        register_output("/referee/robot/buffer_energy", robot_buffer_energy_, 60.0);
+        register_output("/referee/game/stage", game_stage_, rmcs_msgs::GameStage::UNKNOWN);
+
+        register_output("/referee/id", robot_id_, rmcs_msgs::RobotId::UNKNOWN);
+        register_output("/referee/shooter/cooling", robot_shooter_cooling_, 0);
+        register_output("/referee/shooter/heat_limit", robot_shooter_heat_limit_, 0);
+        register_output("/referee/chassis_power_limit", robot_chassis_power_limit_, 0.0);
+
+        register_output("/referee/chassis_power", robot_chassis_power_, 0.0);
+        register_output("/referee/buffer_energy", robot_buffer_energy_, 60.0);
+
+        robot_status_watchdog_.reset(5'000);
     }
 
     void update() override {
@@ -63,6 +72,22 @@ public:
                 RCLCPP_WARN(logger_, "Header crc8 invalid");
             }
         }
+
+        if (game_status_watchdog_.tick()) {
+            RCLCPP_INFO(logger_, "Game status receiving timeout. Set stage to unknown.");
+            *game_stage_ = rmcs_msgs::GameStage::UNKNOWN;
+        }
+        if (robot_status_watchdog_.tick()) {
+            RCLCPP_ERROR(logger_, "Robot status receiving timeout. Set to safe indicators.");
+            *robot_shooter_cooling_     = safe_shooter_cooling;
+            *robot_shooter_heat_limit_  = safe_shooter_heat_limit;
+            *robot_chassis_power_limit_ = safe_chassis_power_limit;
+        }
+        if (power_heat_data_watchdog_.tick()) {
+            RCLCPP_ERROR(logger_, "Power heat data receiving timeout. Set to initial values.");
+            *robot_chassis_power_ = 0.0;
+            *robot_buffer_energy_ = 60.0;
+        }
     }
 
 private:
@@ -88,19 +113,35 @@ private:
             update_game_robot_position();
     }
 
-    void update_game_status() {}
+    void update_game_status() {
+        auto& data = reinterpret_cast<GameStatus&>(frame_.body.data);
+
+        *game_stage_ = static_cast<rmcs_msgs::GameStage>(data.game_stage);
+        if (*game_stage_ == rmcs_msgs::GameStage::STARTED)
+            game_status_watchdog_.reset(30'000);
+        else
+            game_status_watchdog_.reset(5'000);
+    }
 
     void update_game_robot_hp() {}
 
     void update_robot_status() {
+        if (*game_stage_ == rmcs_msgs::GameStage::STARTED)
+            robot_status_watchdog_.reset(60'000);
+        else
+            robot_status_watchdog_.reset(5'000);
+
         auto& data = reinterpret_cast<RobotStatus&>(frame_.body.data);
 
+        *robot_id_                  = static_cast<rmcs_msgs::RobotId>(data.robot_id);
         *robot_shooter_cooling_     = data.shooter_barrel_cooling_value;
         *robot_shooter_heat_limit_  = static_cast<int64_t>(1000) * data.shooter_barrel_heat_limit;
         *robot_chassis_power_limit_ = static_cast<double>(data.chassis_power_limit);
     }
 
     void update_power_heat_data() {
+        power_heat_data_watchdog_.reset(3'000);
+
         auto& data            = reinterpret_cast<PowerHeatData&>(frame_.body.data);
         *robot_chassis_power_ = data.chassis_power;
         *robot_buffer_energy_ = static_cast<double>(data.buffer_energy);
@@ -116,14 +157,30 @@ private:
 
     void update_game_robot_position() {}
 
+    // When referee system loses connection unexpectedly,
+    // use these indicators make sure the robot safe.
+    // Muzzle: Cooling priority with level 1
+    static constexpr int64_t safe_shooter_cooling    = 40;
+    static constexpr int64_t safe_shooter_heat_limit = 50'000;
+    // Chassis: Health priority with level 1
+    static constexpr double safe_chassis_power_limit = 45;
+
     rclcpp::Logger logger_;
 
     OutputInterface<serial::Serial> serial_;
     Frame frame_;
     size_t cache_size_ = 0;
 
+    serial_util::TickTimer game_status_watchdog_;
+    OutputInterface<rmcs_msgs::GameStage> game_stage_;
+
+    serial_util::TickTimer robot_status_watchdog_;
+    OutputInterface<rmcs_msgs::RobotId> robot_id_;
     OutputInterface<int64_t> robot_shooter_cooling_, robot_shooter_heat_limit_;
-    OutputInterface<double> robot_chassis_power_, robot_chassis_power_limit_;
+    OutputInterface<double> robot_chassis_power_limit_;
+
+    serial_util::TickTimer power_heat_data_watchdog_;
+    OutputInterface<double> robot_chassis_power_;
     OutputInterface<double> robot_buffer_energy_;
 };
 
